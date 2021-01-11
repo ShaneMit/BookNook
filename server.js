5ea385621d4024a9f4976818a3a21fe6b60ffd51
ShaneMit <shanemit@cox.net>
2021-01-11 15:10:04 -0800

    Init, server.js, gitignore

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
+  useNewURLParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.error(err))
