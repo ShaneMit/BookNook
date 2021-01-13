ff23e54d4c6e5a0f650dcaffc8c45c210f2ac891
ShaneMit <shanemit@cox.net>
2021-01-12 21:27:02 -0800

    Worked on routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
-router.use('/api', require('./gifRoutes.js'))
+router.use('/api', require('./bookRoutes.js'))
+router.use('/api', require('./googleBookRoutes.js'))
 
 module.exports = router
