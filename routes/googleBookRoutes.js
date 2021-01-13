31dd2f8b08d97f3988bd953672a18937370ea1c4
ShaneMit <shanemit@cox.net>
2021-01-12 21:37:14 -0800

    Fixed GB routes

--- routes/googleBookRoutes.js
+++ routes/googleBookRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const axios = require('axios')
 
-router.get('googlebooks/:search', (req, res) => {
+router.get('/googlebooks/:search', (req, res) => {
   axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.search}&key=${process.env.GOOGLE_API_KEY}`)
     .then(({ data }) => {
       res.json(data)
