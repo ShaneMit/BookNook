836cf5247b9b751d056ae28af8c58016efd38b5f
ShaneMit <shanemit@cox.net>
2021-01-13 20:57:07 -0800

    Created get, post, and delete routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router()
+const { Book } = require('../models')
+
+router.get('/books', (req,res) => {
+  Book.find()
+    .then(books => res.json(books))
+    .catch(err => console.error(err))
+})
+
+router.post('/books', (req, res) => {
+  Book.create(req.body)
+    .then(book => res.json(book))
+    .catch(err => console.error(err))
+})
+
+router.delete('/books/:id', (req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
+})
 
 module.exports = router
