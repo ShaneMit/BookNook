c499b7aac8718f5547b0ac4481936d9b50035267
ShaneMit <shanemit@cox.net>
2021-01-16 19:26:28 -0800

    Added search bar on home page and logged book data

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import axios from 'axios'
 
 const Home = () => {
+  const [ bookState, setBookState ] = useState({
+    search: '',
+    books: []
+  })
+
+  bookState.handleInputChange = event => {
+    setBookState({ ...bookState, [event.target.name]: event.target.value })
+  }
+
+  bookState.handleSearchBook = event => {
+    event.preventDefault()
+    axios.get(`/api/googlebooks/${bookState.search}`)
+      .then(({ data }) => {
+        console.log(data)
+      })
+      .catch(err => console.error(err))
+  }
+
   return (
     <>
-      <h1>Home Page</h1>
+      <form onSubmit={bookState.handleSearchBook}>
+        <TextField 
+        label="Search Book" 
+        name="search" 
+        value={bookState.search}
+        onChange={bookState.handleInputChange}/>
+        <Button 
+        variant="outlined" 
+        color="primary"
+        onClick={bookState.handleSearchBook}>
+          Search
+      </Button>
+      </form>
     </>
   )
 }
