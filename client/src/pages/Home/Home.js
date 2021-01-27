fdd1366466edd004e98b11790111d64240d554a9
ShaneMit <shanemit@cox.net>
2021-01-26 18:10:27 -0800

    Working on generating card

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import CardHeader from '@material-ui/core/CardHeader'
 import axios from 'axios'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 400,
+    width: 500
+  },
+});
 
 const Home = () => {
   const [ bookState, setBookState ] = useState({
@@ -18,6 +35,7 @@ const Home = () => {
     axios.get(`/api/googlebooks/${bookState.search}`)
       .then(({ data }) => {
         console.log(data)
+        setBookState({ ...bookState, books: data })
       })
       .catch(err => console.error(err))
   }
@@ -37,6 +55,27 @@ const Home = () => {
           Search
         </Button>
       </form>
+      <div>
+        {
+          bookState.books.map(book => (
+            <Card className={classes.root}>
+                <CardMedia
+                  className={classes.media}
+                  image={book.volumeinfo.imageLinks.thumbnail}
+                  title={book.volumeInfo.title}
+                />
+                <CardHeader
+                  title={book.volumeInfo.title}
+                />
+              <CardActions>
+                <Button size="small" color="primary">
+                  Save
+                </Button>
+              </CardActions>
+            </Card>
+          ))
+        }
+      </div>
     </>
   )
 }
